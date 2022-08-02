057e5c4f7fb50b5db49ebb61933c2068e25c8e59
BhargavN07 <109598494+BhargavN07@users.noreply.github.com>
2022-08-02 13:20:56 +0530

    Add files via upload

---
+++ LoginChecker.java
@@ -0,0 +1,37 @@
+package Phase1;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginChecker")
+public class LoginChecker extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public LoginChecker() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println(config.getServletName());
+	}
+
+	
+	public void destroy() {
+		System.out.println("Distroy servlet Object");
+	}
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Service Method");
+	}
+
+}
---
+++ Servlet.java
@@ -0,0 +1,48 @@
+package Phase1;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
